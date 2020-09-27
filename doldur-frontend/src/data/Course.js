8340bd070db2910d607886f49fffb154a3254b36
eren <erenerisken@gmail.com>
2020-09-27 19:21:51 +0300

    Created a template for courses

---
+++ doldur-frontend/src/data/Course.js
@@ -0,0 +1,87 @@
+export const exampleCourses = [
+    {
+        code: 5710213,
+        abbreviation: "CENG213",
+        name: "Data Structures",
+        sections: [
+            {
+                instructor: "Yusuf Sahillioğlu",
+                dept: ["CENG", "EE"],
+                surnameStart: "AA",
+                surnameEnd: "FF",
+                lectureTimes: [
+                    {
+                        classroom: "BMB-1",
+                        day: 0,
+                        startHour: 8,
+                        startMin: 40,
+                        endHour: 10,
+                        endMin: 30
+                    },
+                    {
+                        classroom: "BMB-4",
+                        day: 2,
+                        startHour: 15,
+                        startMin: 40,
+                        endHour: 17,
+                        endMin: 30
+                    }
+                ]
+            },
+            {
+                instructor: "Cevat Şener",
+                dept: ["CENG", "EE"],
+                surnameStart: "FG",
+                surnameEnd: "ZZ",
+                lectureTimes: [
+                    {
+                        classroom: "U-3",
+                        day: 2,
+                        startHour: 10,
+                        startMin: 40,
+                        endHour: 12,
+                        endMin: 30
+                    },
+                    {
+                        classroom: "CZ-14",
+                        day: 6,
+                        startHour: 15,
+                        startMin: 40,
+                        endHour: 17,
+                        endMin: 30
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        code: 5710140,
+        abbreviation: "CENG140",
+        name: "C Programming",
+        sections: [
+            {
+                instructor: "Göktürk Üçoluk",
+                dept: ["CENG"],
+                surnameStart: "AA",
+                surnameEnd: "ZZ",
+                lectureTimes: [
+                    {
+                        classroom: "BMB-1",
+                        day: 0,
+                        startHour: 8,
+                        startMin: 40,
+                        endHour: 10,
+                        endMin: 30
+                    },
+                    {
+                        day: 2,
+                        startHour: 15,
+                        startMin: 40,
+                        endHour: 17,
+                        endMin: 30
+                    }
+                ]
+            }
+        ]
+    }
+]
