043a526c254e02ba2b10d6b53f1300f7817e912b
Umut Deniz <umut.deniz@inofab.health>
2025-01-31 10:14:51 +0300

    Changed Welcome Dialog

--- doldur-frontend/src/WelcomeDialog.js
+++ doldur-frontend/src/WelcomeDialog.js
@@ -1,44 +1,85 @@
 import React from "react";
 import {
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    Button
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+  Tooltip,
+  Box,
+  Typography,
 } from "@material-ui/core";
 
-export class WelcomeDialog extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: true
-        }
-    }
-    render() {
-        return (
-        <div className={"dialog-wrapper"}>
-            <Dialog open={this.state.open}
-                    onClose={() => this.setState({open: false})}
-                    aria-labelledby={"alert-dialog-title"}
-                    aria-describedby={"alert-dialog-description"}>
-                <DialogTitle id={"alert-dialog-title"}>
-                    Welcome!
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id={"alert-dialog-description"}>
-                        Bir süredir yoktum demişler öldü, şimdi yazsınlar bakalım kral geri döndü
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => this.setState({open: false})}
-                            color={"secondary"}
-                            variant={"contained"}
-                            autoFocus={false}>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>);
-    }
+export class WelcomeDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+      copied: false,
+    };
+  }
+
+  handleCopyEmail = () => {
+    navigator.clipboard.writeText("info.robotdegilim@gmail.com");
+    this.setState({ copied: true });
+
+    setTimeout(() => {
+      this.setState({ copied: false });
+    }, 1500);
+  };
+
+  render() {
+    return (
+      <div className="dialog-wrapper">
+        <Dialog
+          open={this.state.open}
+          onClose={() => this.setState({ open: false })}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Welcome!</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Öneriler ve şikayetler için{" "}
+              <Tooltip
+                title={this.state.copied ? "Copied" : "Click to copy"}
+                open={this.state.copied}
+                arrow
+              >
+                <Box
+                  component="span"
+                  sx={{
+                    cursor: "pointer",
+                    fontWeight: "bold",
+                    color: "blue",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                    "&:hover": { textDecoration: "underline" },
+                  }}
+                  onClick={this.handleCopyEmail}
+                >
+                  <Typography style={{ color: "blue" }}>
+                    {" "}
+                    info.robotdegilim@gmail.com{" "}
+                  </Typography>
+                </Box>
+              </Tooltip>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => this.setState({ open: false })}
+              color="secondary"
+              variant="contained"
+              autoFocus={false}
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
 }
