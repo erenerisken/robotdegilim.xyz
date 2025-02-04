5e86cd18d7a1bcc1a468b35d35d15ec985d9b9d0
Umut Deniz <umut.deniz@inofab.health>
2025-02-04 11:38:33 +0300

    changed colorset and add color picker to course advanced settings

--- doldur-frontend/src/CourseAdvancedSettings.js
+++ doldur-frontend/src/CourseAdvancedSettings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionSummary,
@@ -7,11 +7,14 @@ import {
   FormControlLabel,
   Checkbox,
   Divider,
-  Button,
+  Menu,
+  MenuItem,
+  IconButton,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PaletteIcon from "@material-ui/icons/Palette";
 import TuneIcon from "@material-ui/icons/Tune";
+import { Colorset } from "./Colorset";
 
 import "./CourseAdvancedSettings.css";
 
@@ -19,8 +22,7 @@ export const CourseAdvancedSettings = ({
   settings,
   onSettingsChange,
   color,
-  onPreviousColor,
-  onNextColor,
+  onColorChange,
 }) => {
   const handleChange = (field) => {
     onSettingsChange({
@@ -28,7 +30,21 @@ export const CourseAdvancedSettings = ({
       [field]: !settings[field],
     });
   };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const colorset = new Colorset();
+  const colors = colorset.colors;
+  const open = Boolean(anchorEl);
 
+  const handleOpenColorPalette = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleColorChange = (color) => {
+    onColorChange(color);
+    handleClose();
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className="course-settings-wrapper">
       <Accordion style={{ background: color.secondary }}>
@@ -89,25 +105,61 @@ export const CourseAdvancedSettings = ({
               />
             </div>
             <Divider />
-            <div className="settings-row">
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<PaletteIcon />}
-                style={{ marginTop: "6pt" }}
-                onClick={onPreviousColor}
+            <div className="settings-row" style={{display:"flex" , alignItems:"center"}}>
+              <IconButton
+                style={{ padding: 0 }}
+                id="palette-button"
+                aria-controls={open ? "palette-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleOpenColorPalette}
               >
-                Prev Color
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<PaletteIcon />}
-                style={{ marginTop: "6pt", marginLeft: "6pt" }}
-                onClick={onNextColor}
+                <PaletteIcon fontSize="20" style={{ color: "black" }} />
+                <Typography style={{ color: "black" }}>Change Color</Typography>
+              </IconButton>
+              <Menu
+                id="palette-menu"
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "palette-button",
+                }}
+                PaperProps={{
+                  style: {
+                    backgroundColor: "black",
+                  },
+                }}
               >
-                Next Color
-              </Button>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(3, 1fr)",
+                    gap: "5px",
+                    padding: "5px",
+                    paddingTop: "0px",
+                    paddingBottom: "0px",
+                    backgroundColor: "black",
+                  }}
+                >
+                  {colors.map((color) => (
+                    <MenuItem
+                      key={color.main}
+                      onClick={() => handleColorChange(color)}
+                      style={{ padding: 0 }}
+                    >
+                      <div
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                          backgroundColor: color.main,
+                          borderRadius: "4px",
+                        }}
+                      ></div>
+                    </MenuItem>
+                  ))}
+                </div>
+              </Menu>
             </div>
           </div>
         </AccordionDetails>
