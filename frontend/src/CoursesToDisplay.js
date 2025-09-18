de414758c66404033187b3f3c97e42865e5821a2
Mustafa Alkan <3alkan@gmail.com>
2025-09-18 14:02:19 +0300

    remove frontend

--- frontend/src/CoursesToDisplay.js
+++
@@ -1,8 +0,0 @@
-const schedulerData = [
-    { startDate: '2021-02-20T09:40', endDate: '2021-02-20T11:20', title: 'Meeting' },
-    { startDate: '2021-02-14T12:00', endDate: '2021-02-14T13:30', title: 'Go to\n a gym' },
-];
-
-export function getCoursesToDisplay(){
-    return schedulerData;
-}
--- frontend/src/slices/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { appReducer } from "slices/reducers/appReducer";
-
-export default (state, action) => {
-  return appReducer(state, action);
-};
